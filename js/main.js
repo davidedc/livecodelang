6384c6a5307bab5a675204c5aeaa75b920753fd4
Guy John <guy@rumblesan.com>
2013-11-03 13:06:54 +0000

    Setting up require js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*jslint browser: true */
+/*global require */
+
+'use strict';
+
+require(
+    [
+        'parser'
+    ],
+    function (
+        parser
+    ) {
+        window.parser = parser;
+    }
+);
+
